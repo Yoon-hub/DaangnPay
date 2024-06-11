8815e21f4cf8889474df32e60af200d231752fcc
yoon <kong4170@naver.com>
2024-06-11 22:03:16 +0900

    Feat: CommonViewController 추가

---
+++ DaangnPay/Common/CommonVC/CommonViewController.swift
@@ -0,0 +1,64 @@
+//
+//  CommonViewController.swift
+//  DaangnPay
+//
+//  Created by 최윤제 on 6/11/24.
+//
+
+import UIKit
+import Combine
+
+class CommonViewController<ViewModel: ViewModelable>: UIViewController, Presentable {
+    
+    typealias ViewModelType = ViewModel
+    
+    let viewModel: ViewModelType
+    
+    var statePublisher: AnyPublisher<ViewModel.State, Never> {
+        stateSubject.eraseToAnyPublisher()
+    }
+    
+    private var stateSubject = PassthroughSubject<ViewModel.State, Never>()
+    
+    var cancellables = Set<AnyCancellable>()
+    
+    init(viewModel: ViewModelType) {
+        self.viewModel = viewModel
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    // MARK: - View Life Cycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        set()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func handleOutput(_ state: ViewModelType.State) { }
+    
+    // MARK: - Set
+    func set() {
+        setUI()
+        viewModelOutPutbind()
+        setNavigationBar()
+    }
+    
+    func setUI() {view.backgroundColor = .white}
+    func setNavigationBar() {}
+    
+    // MARK: - ViewModel Interaction
+    private func viewModelOutPutbind() {
+        viewModel.output
+            .sink { [weak self] state in self?.stateSubject.send(state) }
+            .store(in: &cancellables)
+        
+        stateSubject
+            .sink { [weak self] state in
+                self?.handleOutput(state)
+            }
+            .store(in: &cancellables)
+    }
+}
