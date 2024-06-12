53bd3ea441026c56d482d70804302b38046de845
yoon <kong4170@naver.com>
2024-06-12 22:15:09 +0900

    Feat: MockURLRotocol

---
+++ DaangnPayTests/NetworkTests/APIServiceTests.swift
@@ -0,0 +1,8 @@
+//
+//  APIServiceTests.swift
+//  DaangnPayTests
+//
+//  Created by 최윤제 on 6/12/24.
+//
+
+import Foundation
---
+++ DaangnPayTests/NetworkTests/Mock/MockURLProtocol.swift
@@ -0,0 +1,37 @@
+//
+//  MockURLProtocol.swift
+//  DaangnPayTests
+//
+//  Created by 최윤제 on 6/12/24.
+//
+
+import Foundation
+
+class MockURLProtocol: URLProtocol {
+    static var requestHandler: ((URLRequest) throws -> (HTTPURLResponse, Data))?
+    
+    override class func canInit(with request: URLRequest) -> Bool {
+        return true
+    }
+    
+    override class func canonicalRequest(for request: URLRequest) -> URLRequest {
+        return request
+    }
+    
+    override func startLoading() {
+        guard let handler = MockURLProtocol.requestHandler else {
+            fatalError("Request handler is not set.")
+        }
+        
+        do {
+            let (response, data) = try handler(request)
+            client?.urlProtocol(self, didReceive: response, cacheStoragePolicy: .notAllowed)
+            client?.urlProtocol(self, didLoad: data)
+            client?.urlProtocolDidFinishLoading(self)
+        } catch {
+            client?.urlProtocol(self, didFailWithError: error)
+        }
+    }
+    
+    override func stopLoading() {}
+}
