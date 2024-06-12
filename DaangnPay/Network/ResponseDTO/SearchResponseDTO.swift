007b4aded75b000643ce2453fb93f64f32fe1b01
yoon <kong4170@naver.com>
2024-06-12 14:27:42 +0900

    Feat: Search API DTO 추가

---
+++ DaangnPay/Network/ResponseDTO/SearchResponseDTO.swift
@@ -0,0 +1,24 @@
+//
+//  SearchResponseDTO.swift
+//  DaangnPay
+//
+//  Created by 최윤제 on 6/12/24.
+//
+
+import Foundation
+
+struct SearchResponseDTO: Decodable {
+    let error: String
+    let total: String
+    let page: String
+    let books: [Book]
+}
+
+struct Book: Decodable {
+    let title: String
+    let subtitle: String?
+    let isbn13: String
+    let price: String
+    let image: String
+    let url: String
+}
