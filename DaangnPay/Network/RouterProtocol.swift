a246ba907e25ea43346aa21e3c53b8b9e4e7fe96
yoon <kong4170@naver.com>
2024-06-12 09:31:01 +0900

    Feat: RouterProtocol추가

---
+++ DaangnPay/Network/RouterProtocol.swift
@@ -0,0 +1,22 @@
+//
+//  RouterProtocol.swift
+//  DaangnPay
+//
+//  Created by 최윤제 on 6/12/24.
+//
+
+import Foundation
+
+enum HTTPMethod: String {
+  case get = "GET"
+  case post = "POST"
+  case put = "PUT"
+  case delete = "DELETE"
+}
+protocol RouterProtocol {
+  var endPoint: String { get }
+  var method: HTTPMethod { get }
+  var headers: [String: String]? { get }
+  var parameters: [String: Any]? { get }
+  var queryItems: [String: String]? { get }
+}
