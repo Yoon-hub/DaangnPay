a9965a982783208b5c94ccb7fac235b6ae372f83
yoon <kong4170@naver.com>
2024-06-13 10:45:42 +0900

    Feat: APIServiceStub 추가

---
+++ DaangnPayTests/ViewModelTests/Mock/APIServiceStub.swift
@@ -0,0 +1,31 @@
+//
+//  APIServiceStub.swift
+//  DaangnPayTests
+//
+//  Created by 최윤제 on 6/13/24.
+//
+
+import Foundation
+@testable import DaangnPay
+
+final class APIServiceStub: APIServiceProtocol {
+    
+    private let error: Error?
+    private let data: Decodable?
+    
+    init(error: Error? = nil, data: Decodable? = nil) {
+        self.error = error
+        self.data = data
+    }
+    
+    func apiRequest<T: Decodable, Router: RouterProtocol>(type: T.Type, router: Router) async throws -> T {
+        
+        if let error { throw error }
+        
+        guard let data = data as? T else {
+            throw URLError(.badServerResponse)
+        }
+        
+       return data
+    }
+}
